bcb70018285a9c7972a80935d3a8f6fd44e75322
Paulo Gräbin <plgrabin@gmail.com>
2020-01-22 23:19:15 -0300

    Add new entity to store user's Roles

---
+++ src/main/java/com/paulograbin/confirmation/Role.java
@@ -0,0 +1,25 @@
+package com.paulograbin.confirmation;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    private RoleName name;
+
+    public Role() {
+    }
+}
---
+++ src/main/java/com/paulograbin/confirmation/RoleName.java
@@ -0,0 +1,7 @@
+package com.paulograbin.confirmation;
+
+public enum RoleName {
+    ROLE_USER,
+    ROLE_MC,
+    ROLE_ADMIN
+}
