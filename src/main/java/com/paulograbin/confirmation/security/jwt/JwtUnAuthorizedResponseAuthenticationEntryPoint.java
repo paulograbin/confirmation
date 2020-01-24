fe150caf5556fbf654cb9446b7750e77bd5f4b1f
Paulo Gräbin <plgrabin@gmail.com>
2020-01-24 00:46:59 -0300

    Add comment with explanation of what this class does. Add logger.

--- src/main/java/com/paulograbin/confirmation/security/jwt/JwtUnAuthorizedResponseAuthenticationEntryPoint.java
+++ src/main/java/com/paulograbin/confirmation/security/jwt/JwtUnAuthorizedResponseAuthenticationEntryPoint.java
@@ -1,5 +1,8 @@
 package com.paulograbin.confirmation.security.jwt;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
@@ -14,9 +17,17 @@ public class JwtUnAuthorizedResponseAuthenticationEntryPoint implements Authenti
 
     private static final long serialVersionUID = -8970718410437077606L;
 
+    private static final Logger log = LoggerFactory.getLogger(JwtUnAuthorizedResponseAuthenticationEntryPoint.class);
+
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, org.springframework.security.core.AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
-                "You would need to provide the Jwt Token to Access This resource");
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        // This is invoked when user tries to access a secured REST resource without supplying any credentials
+        // We should just send a 401 Unauthorized response because there is no 'login page' to redirect to
+
+        log.info("Responding with unauthorized error. Message {}", authException.getMessage());
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "You would need to provide the Jwt Token to Access This resource");
     }
 }
