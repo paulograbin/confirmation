b671ccf2171353b7e6b8a004c3d716bd018f912c
Paulo Gräbin <plgrabin@gmail.com>
2020-02-10 23:53:32 -0300

    Add method to find participation based on the event and user id

--- src/main/java/com/paulograbin/confirmation/persistence/ParticipationRepository.java
+++ src/main/java/com/paulograbin/confirmation/persistence/ParticipationRepository.java
@@ -2,7 +2,14 @@ package com.paulograbin.confirmation.persistence;
 
 import com.paulograbin.confirmation.Participation;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 
+@Repository
 public interface ParticipationRepository extends CrudRepository<Participation, Long> {
+
+    Optional<Participation> findByEventIdAndUserId(long eventId, long userId);
+
 }
