0a6c3844bbd575c759c47ea3dd20d43d79624314
Paulo Gräbin <plgrabin@gmail.com>
2020-02-10 23:53:17 -0300

    Add method to fetch every event from a creator based on the creator id

--- src/main/java/com/paulograbin/confirmation/persistence/EventRepository.java
+++ src/main/java/com/paulograbin/confirmation/persistence/EventRepository.java
@@ -3,6 +3,12 @@ package com.paulograbin.confirmation.persistence;
 import com.paulograbin.confirmation.Event;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 
 public interface EventRepository extends CrudRepository<Event, Long> {
+
+    List<Event> findAllByCreatorId(long creatorId);
+
 }
