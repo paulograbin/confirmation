875837b3b1126052c8ce9cd8ccf923202ceb4331
Paulo Gräbin <plgrabin@gmail.com>
2020-02-20 00:57:18 -0300

    Add method to get role by name from database

--- src/main/java/com/paulograbin/confirmation/service/RoleService.java
+++ src/main/java/com/paulograbin/confirmation/service/RoleService.java
@@ -1,12 +1,16 @@
 package com.paulograbin.confirmation.service;
 
 import com.paulograbin.confirmation.Role;
+import com.paulograbin.confirmation.RoleName;
+import com.paulograbin.confirmation.exception.NotFoundException;
 import com.paulograbin.confirmation.persistence.RoleRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+
+import static java.lang.String.format;
 
 
 @Service
@@ -20,5 +24,10 @@ public class RoleService {
 
     public Role save(Role roleToSave) {
         return roleRepository.save(roleToSave);
+    }
+
+    public Role getByName(RoleName role) {
+        return roleRepository.findByName(role)
+                .orElseThrow(() -> new NotFoundException(format("Role %s not found", role.toString())));
     }
 }
