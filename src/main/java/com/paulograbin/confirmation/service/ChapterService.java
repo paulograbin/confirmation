758ea3dafa1f54a39afb7998aa46bb1840312e21
Paulo Gräbin <plgrabin@gmail.com>
2020-02-25 16:58:23 -0300

    Add feature to create Chapter

--- src/main/java/com/paulograbin/confirmation/service/ChapterService.java
+++ src/main/java/com/paulograbin/confirmation/service/ChapterService.java
@@ -3,6 +3,7 @@ package com.paulograbin.confirmation.service;
 import com.paulograbin.confirmation.domain.Chapter;
 import com.paulograbin.confirmation.exception.NotFoundException;
 import com.paulograbin.confirmation.persistence.ChapterRepository;
+import com.paulograbin.confirmation.web.ChapterCreationRequest;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -17,5 +18,13 @@ public class ChapterService {
     public Chapter fetchById(long chapterId) {
         return chapterRepository.findById(chapterId)
                 .orElseThrow(() -> new NotFoundException("Chapter " + chapterId + " not found!"));
+    }
+
+    public Chapter createChapter(ChapterCreationRequest creationRequest) {
+        Chapter chapterToCreate = new Chapter();
+        chapterToCreate.setId(creationRequest.getId());
+        chapterToCreate.setName(creationRequest.getName());
+
+        return chapterRepository.save(chapterToCreate);
     }
 }
--- src/main/java/com/paulograbin/confirmation/web/ChapterController.java
+++ src/main/java/com/paulograbin/confirmation/web/ChapterController.java
@@ -1,24 +1,59 @@
 package com.paulograbin.confirmation.web;
 
 import com.paulograbin.confirmation.domain.Chapter;
+import com.paulograbin.confirmation.domain.User;
 import com.paulograbin.confirmation.persistence.ChapterRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.paulograbin.confirmation.service.ChapterService;
+import com.paulograbin.confirmation.web.dto.ChapterDTO;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.internal.util.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
 @RequestMapping(value = "/chapter")
 public class ChapterController {
 
+    private static final Logger log = LoggerFactory.getLogger(ChapterController.class);
+
+    @Resource
+    private ChapterService chapterService;
+
     @Resource
     private ChapterRepository chapterRepository;
 
+    @Resource
+    private ModelMapper modelMapper;
+
     @GetMapping
-    public Iterable<Chapter> fetchAllChapters() {
-        return chapterRepository.findAll();
+    public List<ChapterDTO> fetchAllChapters() {
+        log.info("All chapter");
+
+        Iterable<Chapter> eventIterator = chapterRepository.findAll();
+        List<Chapter> arrayList = Lists.from(eventIterator.iterator());
+
+        return arrayList.stream()
+                .map(u -> modelMapper.map(u, ChapterDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ChapterDTO createNewChapter(@Valid @RequestBody ChapterCreationRequest creationRequest) {
+        log.info("Creating new chapter {}", creationRequest);
+
+        Chapter createdChapter = chapterService.createChapter(creationRequest);
+
+        return modelMapper.map(createdChapter, ChapterDTO.class);
     }
 
 }
