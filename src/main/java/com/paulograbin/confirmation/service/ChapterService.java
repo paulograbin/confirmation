d7a511b5e7c72221a2483c3d559d165c51de3b56
Paulo Gräbin <paulo.grabin@sap.com>
2020-04-11 18:06:43 -0300

    Validate if chapter exists before creating it

--- src/main/java/com/paulograbin/confirmation/service/ChapterService.java
+++ src/main/java/com/paulograbin/confirmation/service/ChapterService.java
@@ -4,6 +4,8 @@ import com.paulograbin.confirmation.domain.Chapter;
 import com.paulograbin.confirmation.exception.NotFoundException;
 import com.paulograbin.confirmation.persistence.ChapterRepository;
 import com.paulograbin.confirmation.web.ChapterCreationRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -12,22 +14,32 @@ import javax.annotation.Resource;
 @Service
 public class ChapterService {
 
+    private static final Logger log = LoggerFactory.getLogger(ChapterService.class);
+
     @Resource
     private ChapterRepository chapterRepository;
 
     public Chapter fetchById(long chapterId) {
+        log.info("Fetching chapter {}", chapterId);
+
         return chapterRepository.findById(chapterId)
                 .orElseThrow(() -> new NotFoundException("Chapter " + chapterId + " not found!"));
     }
 
     public Chapter createChapter(ChapterCreationRequest creationRequest) {
-        // validate if chapter already exist
-
         Chapter chapterToCreate = new Chapter();
         chapterToCreate.setId(creationRequest.getId());
         chapterToCreate.setName(creationRequest.getName());
 
+        validateChapterNumberAvailable(chapterToCreate.getId());
+
         return chapterRepository.save(chapterToCreate);
+    }
+
+    private void validateChapterNumberAvailable(final Long id) {
+        if (chapterRepository.existsById(id)) {
+            throw new RuntimeException("Chapter id " + id + " is already taken");
+        }
     }
 
     public long count() {
