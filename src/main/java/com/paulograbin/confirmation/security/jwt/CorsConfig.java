b49527642f035f2a3ec301db5667668398ef5bf0
Paulo Gräbin <paulo.grabin@sap.com>
2020-05-02 15:37:57 -0300

    Add cors allowed origin

---
+++ src/main/java/com/paulograbin/confirmation/security/jwt/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.paulograbin.confirmation.security.jwt;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfiguration corsConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("https://paulograbin.github.io/confirmationFrontend/*");
+
+        return corsConfiguration;
+    }
+
+}
