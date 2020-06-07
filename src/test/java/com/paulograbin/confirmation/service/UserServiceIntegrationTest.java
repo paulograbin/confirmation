5f1acbb9eb596cba3822e5f3b53aac8e177afe70
Paulo Gräbin <plgrabin@gmail.com>
2020-06-07 19:11:47 -0300

    More tests

--- src/test/java/com/paulograbin/confirmation/service/UserServiceIntegrationTest.java
+++ src/test/java/com/paulograbin/confirmation/service/UserServiceIntegrationTest.java
@@ -1,10 +1,14 @@
 package com.paulograbin.confirmation.service;
 
 import com.paulograbin.confirmation.domain.User;
+import com.paulograbin.confirmation.exception.NotFoundException;
+import com.paulograbin.confirmation.exception.UsernameNotAvailableException;
 import com.paulograbin.confirmation.persistence.UserRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,11 +16,15 @@ import javax.annotation.Resource;
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.api.Assertions.fail;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserServiceIntegrationTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceIntegrationTest.class);
 
     @Resource
     UserService userService;
@@ -24,35 +32,67 @@ public class UserServiceIntegrationTest {
     @Resource
     UserRepository userRepository;
 
-    @Before
-    public void setUp() throws Exception {
-    }
-
     @Test
     public void contextLoads() {
         assertThat(userService).isNotNull();
     }
 
+    @Before
+    public void setUp() throws Exception {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void duringCreation_userReceivesId() {
+        User userToCreate = makeUser();
+
+        createUser(userToCreate);
+
+        assertThat(userToCreate.getId()).isNotNull();
+        assertThat(userToCreate.getCreationDate()).isNotNull();
+        assertThat(userToCreate.getRoles()).isNotEmpty();
+    }
+
     @Test
     public void givenValidUser__whenCreatingUser__shouldBeCreated() {
-        User user = makeUser();
-
-        user = userService.createUser(user);
+        User user = createUser(makeUser());
 
         User returnedUser = userService.fetchById(user.getId());
 
         assertThat(returnedUser).isNotNull();
     }
 
-//    @Test(expected = UsernameNotAvailableException.class)
-//    public void givenAlreadyTakenUsername__whenCreatingUser__shouldThrowException() {
-//        User user = makeUser();
-//
-//        userService.createUser(user);
-//        userService.createUser(user);
-//
-//        fail("Should throw exception before reaching this line");
-//    }
+    @Test
+    public void givenValidId_whenFindingUserById_mustReturnUser() {
+        User user = createUser(makeUser());
+
+        User returnedUser = userService.fetchById(user.getId());
+
+        assertThat(returnedUser).isNotNull();
+    }
+
+
+    @Test
+    public void givenInvalidId_whenFindingUserById_mustThrowException() {
+        Throwable throwable = catchThrowable(() -> userService.fetchById(323232L));
+
+        assertThat(throwable).isInstanceOf(NotFoundException.class);
+    }
+
+
+    private User createUser(User user) {
+        return userService.createUser(user);
+    }
+
+    @Test(expected = UsernameNotAvailableException.class)
+    public void givenAlreadyTakenUsername__whenCreatingUser__shouldThrowException() {
+        User user = makeUser();
+
+        userService.createUser(user);
+        userService.createUser(user);
+
+        fail("Should throw exception before reaching this line");
+    }
 
     private User makeUser() {
         int randomNumber = new Random().nextInt(1000);
@@ -66,4 +106,5 @@ public class UserServiceIntegrationTest {
         user.setPassword("123" + randomNumber);
         return user;
     }
+
 }
