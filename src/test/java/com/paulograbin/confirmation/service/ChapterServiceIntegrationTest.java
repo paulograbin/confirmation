00a32becd686eb32afae45be69c93ce2c471368a
Paulo Gräbin <plgrabin@gmail.com>
2020-07-20 00:36:49 -0300

    More integration tests for chapter service

--- src/test/java/com/paulograbin/confirmation/service/ChapterServiceIntegrationTest.java
+++ src/test/java/com/paulograbin/confirmation/service/ChapterServiceIntegrationTest.java
@@ -1,27 +1,41 @@
 package com.paulograbin.confirmation.service;
 
 import com.paulograbin.confirmation.domain.Chapter;
+import com.paulograbin.confirmation.domain.Event;
 import com.paulograbin.confirmation.domain.User;
 import com.paulograbin.confirmation.persistence.ChapterRepository;
 import com.paulograbin.confirmation.usecases.ChapterCreationRequest;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import javax.transaction.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @SpringBootTest
+@Transactional
 class ChapterServiceIntegrationTest {
 
     @Resource
     ChapterService chapterService;
 
+    @Resource
+    UserService userService;
+
+    @Resource
+    EventService eventService;
 
     @Resource
     ChapterRepository chapterRepository;
+
+    private Chapter chapter;
 
 
     @Test
@@ -30,12 +44,11 @@ class ChapterServiceIntegrationTest {
         assertThat(chapterRepository).isNotNull();
     }
 
-
     @Test
     public void duringCreation_userReceivesId() {
         ChapterCreationRequest request = new ChapterCreationRequest(100L, "Chapter Test");
 
-        Chapter chapter = chapterService.createChapter(request);
+        chapter = chapterService.createChapter(request);
 
         assertThat(chapter).isNotNull();
         assertThat(chapter.getId()).isNotNull();
@@ -44,10 +57,11 @@ class ChapterServiceIntegrationTest {
     @Test
     public void givenValidUser__whenCreatingUser__shouldBeCreated() {
         ChapterCreationRequest request = new ChapterCreationRequest(105L, "Chapter Test");
-        Chapter createdChapter = chapterService.createChapter(request);
 
-        assertThat(createdChapter).isNotNull();
-        assertThat(createdChapter.getId()).isNotNull();
+        chapter = chapterService.createChapter(request);
+
+        assertThat(chapter).isNotNull();
+        assertThat(chapter.getId()).isNotNull();
     }
 
     @Test
@@ -55,18 +69,72 @@ class ChapterServiceIntegrationTest {
         ChapterCreationRequest request = new ChapterCreationRequest(55L, "Chapter Test");
         chapterService.createChapter(request);
 
-        Chapter returnedChapter = chapterService.fetchById(55L);
+        Chapter chapter = chapterService.fetchById(55L);
 
-        assertThat(returnedChapter).isNotNull();
+        assertThat(chapter).isNotNull();
     }
 
     @Test
-    void name2() {
+    void chapterNumberNotAvailable() {
         ChapterCreationRequest request = new ChapterCreationRequest(56L, "Chapter Test");
         chapterService.createChapter(request);
 
-        Assertions.assertThrows(RuntimeException.class, () -> {
-            chapterService.createChapter(request);
-        });
+        assertThrows(RuntimeException.class,
+                () -> chapterService.createChapter(request));
+    }
+
+    @Test
+    void chapterCreatedHasNoEvent() {
+        ChapterCreationRequest request = new ChapterCreationRequest(105L, "Chapter Test");
+
+        chapter = chapterService.createChapter(request);
+
+        assertThat(chapter.getEvents()).isEmpty();
+    }
+
+    @Test
+    void chapterHasEventAfterEventIsCreated() {
+        ChapterCreationRequest request = new ChapterCreationRequest(105L, "Chapter Test");
+        chapter = chapterService.createChapter(request);
+
+        Event event = makeEvent();
+        event = eventService.createEvent(event, event.getCreator());
+
+        assertThat(event.getChapter().getId()).isEqualTo(request.getId());
+        assertThat(chapter.getEvents()).hasSize(1);
+    }
+
+    private Event makeEvent() {
+        int randomNumber = new Random().nextInt(1000);
+
+        var event = new Event();
+
+        event.setCreator(makeUserCreator());
+        event.setTitle("Test event " + randomNumber);
+        event.setDescription("Test event description " + randomNumber);
+        event.setAddress("Test event address " + randomNumber);
+        event.setDate(LocalDate.now().plusDays(1));
+        event.setTime(LocalTime.now());
+        event.setPublished(false);
+
+        return event;
+    }
+
+    private User makeUserCreator() {
+        int randomNumber = new Random().nextInt(1000);
+
+        User user = new User();
+
+        user.setFirstName("Paulo" + randomNumber);
+        user.setLastName("Grabin" + randomNumber);
+        user.setUsername("plgrabin" + randomNumber);
+        user.setEmail("plgrabin@gmail.com" + randomNumber);
+        user.setPassword("123" + randomNumber);
+
+        user.setChapter(chapter);
+
+        userService.createUser(user);
+
+        return user;
     }
 }
