5cea6ba5d700291de3bfa84f46ae890501758001
Paulo Gräbin <plgrabin@gmail.com>
2020-08-11 02:07:43 -0300

    New endpoint to fetch details about a specific Chapter. Add list of members to ChapterDTO.

--- src/main/java/com/paulograbin/confirmation/web/ChapterController.java
+++ src/main/java/com/paulograbin/confirmation/web/ChapterController.java
@@ -5,18 +5,27 @@ import com.paulograbin.confirmation.persistence.ChapterRepository;
 import com.paulograbin.confirmation.service.ChapterService;
 import com.paulograbin.confirmation.usecases.ChapterCreationRequest;
 import com.paulograbin.confirmation.web.dto.ChapterDTO;
+import com.paulograbin.confirmation.web.dto.UserDTO;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.internal.util.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.lang.String.format;
 
 
 @RestController
@@ -44,6 +53,21 @@ public class ChapterController {
         return arrayList.stream()
                 .map(u -> modelMapper.map(u, ChapterDTO.class))
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ChapterDTO listChapter(@PathVariable long id) {
+        log.info(format("Fetching chapter %d", id));
+
+        Chapter chapter = chapterService.fetchById(id);
+        ChapterDTO chapterDTO = modelMapper.map(chapter, ChapterDTO.class);
+
+        chapterDTO.setMembers(chapter.getUsers().stream()
+                .map(u -> modelMapper.map(u, UserDTO.class))
+                .collect(Collectors.toList()));
+
+        return chapterDTO;
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
--- src/main/java/com/paulograbin/confirmation/web/dto/ChapterDTO.java
+++ src/main/java/com/paulograbin/confirmation/web/dto/ChapterDTO.java
@@ -2,11 +2,14 @@ package com.paulograbin.confirmation.web.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 
 @Data
 public class ChapterDTO {
 
     private long id;
     private String name;
+    private List<UserDTO> members;
 
 }
