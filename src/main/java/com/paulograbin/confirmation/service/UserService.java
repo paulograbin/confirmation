0b188e1e600e4a0c82921b065b31c3df1198f7c9
Paulo Gräbin <plgrabin@gmail.com>
2020-09-25 01:40:04 -0300

    Remove import

--- src/main/java/com/paulograbin/confirmation/service/UserService.java
+++ src/main/java/com/paulograbin/confirmation/service/UserService.java
@@ -29,7 +29,6 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.transaction.Transactional;
-import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
