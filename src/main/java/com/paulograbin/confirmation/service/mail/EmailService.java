e762402f6f7fe93411c22a1747040e705fad1bfc
Paulo Gräbin <plgrabin@gmail.com>
2020-09-27 01:17:44 -0300

    Include new method to send mail notifying of newly created user request

--- src/main/java/com/paulograbin/confirmation/service/mail/EmailService.java
+++ src/main/java/com/paulograbin/confirmation/service/mail/EmailService.java
@@ -1,9 +1,12 @@
 package com.paulograbin.confirmation.service.mail;
 
 import com.paulograbin.confirmation.domain.User;
+import com.paulograbin.confirmation.domain.UserRequest;
 
 public interface EmailService {
 
     void sendPasswordChangedMail(User userFromDatabase);
+
+    void sendUserRequestCreatedMail(UserRequest userRequest);
 
 }
