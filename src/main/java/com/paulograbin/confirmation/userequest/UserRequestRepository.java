9e9ec6cb4b920cab806c96b466bc26f1fa56fce6
Paulo Gräbin <plgrabin@gmail.com>
2020-10-04 12:23:06 -0300

    Create method to find request by code

--- src/main/java/com/paulograbin/confirmation/userequest/UserRequestRepository.java
+++ src/main/java/com/paulograbin/confirmation/userequest/UserRequestRepository.java
@@ -2,11 +2,14 @@ package com.paulograbin.confirmation.userequest;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRequestRepository extends CrudRepository<UserRequest, Long> {
 
     boolean existsByEmail(String email);
+
+    Optional<UserRequest> findByCode(UUID code);
 
     long countAllByUserNotNull();
 
