9939c07b639c935bfd2e814e54371dd3d56cf5f5
Paulo Gräbin <plgrabin@gmail.com>
2021-09-14 19:16:55 -0300

    Include tests with orElse and orElseGet

---
+++ src/test/java/com/paulograbin/confirmation/featureflag/LambdaTests.java
@@ -0,0 +1,54 @@
+package com.paulograbin.confirmation.featureflag;
+
+import com.paulograbin.confirmation.usecases.user.harddelete.UserHardDeleteUseCase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+
+class LambdaTests {
+
+    private static final Logger logger = LoggerFactory.getLogger(LambdaTests.class);
+
+    @Test
+    void testOneA() {
+        var a = Optional.of("aaa").orElse("aaa");
+        var b = Optional.of("aaa").orElse(test());
+    }
+
+    @Test
+    void testOneB() {
+        Optional.empty().orElseGet(this::test);
+    }
+
+    @Test
+    void testOneAAAA() {
+        Optional.of("aaa").orElseGet(this::test);
+    }
+
+    private String test() {
+        logger.info("Entrou aqui ehein...");
+
+        return "OrElse";
+    }
+
+    @Test
+    void testThree() {
+        String user = makeEmptyOptional().orElse("Executing orElse");
+        System.out.println(user);
+        String user2 = makeEmptyOptional().orElseGet(() -> "Executing orElse");
+        System.out.println(user2);
+    }
+
+    private Optional<String> makeNonOptional() {
+        return Optional.of("Present!!");
+    }
+
+
+
+    private Optional<String> makeEmptyOptional() {
+        return Optional.empty();
+    }
+}
