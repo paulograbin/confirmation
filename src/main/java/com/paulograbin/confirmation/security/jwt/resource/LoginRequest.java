ba37e189b15309742cbe2d615596f9ee2b0067bb
Paulo Grolli Grabin <paulograbin@gmail.com>
2022-12-19 13:50:33 -0300

    Remove password from toString

--- src/main/java/com/paulograbin/confirmation/security/jwt/resource/LoginRequest.java
+++ src/main/java/com/paulograbin/confirmation/security/jwt/resource/LoginRequest.java
@@ -1,25 +1,21 @@
 package com.paulograbin.confirmation.security.jwt.resource;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
-
+@Getter
+@Setter
+@NoArgsConstructor
 public class LoginRequest {
 
-    @Getter
-    @Setter
     private String usernameOrEmail;
-
-    @Getter
-    @Setter
     private String password;
 
     @Override
     public String toString() {
         return "LoginRequest{" +
-                "usernameOrEmail='" + usernameOrEmail + '\'' +
-                ", password='" + password + '\'' +
-                '}';
+                "usernameOrEmail='" + usernameOrEmail + '\'' + '}';
     }
 }
