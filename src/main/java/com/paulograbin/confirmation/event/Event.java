f261b26efc01aeb02882ebd6fdf19dead6170ab4
Paulo Grolli Grabin <paulograbin@gmail.com>
2022-12-19 10:57:31 -0300

    Adapt to Spring Boot 3

--- src/main/java/com/paulograbin/confirmation/event/Event.java
+++ src/main/java/com/paulograbin/confirmation/event/Event.java
@@ -5,10 +5,19 @@ import com.paulograbin.confirmation.chapter.Chapter;
 import com.paulograbin.confirmation.domain.AbstracEntity;
 import com.paulograbin.confirmation.user.User;
 import com.paulograbin.confirmation.participation.Participation;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
