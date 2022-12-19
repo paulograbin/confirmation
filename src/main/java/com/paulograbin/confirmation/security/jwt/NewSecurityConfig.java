68327c6d3bfe618036788b775522c166f1df0e1b
Paulo Grolli Grabin <paulograbin@gmail.com>
2022-12-19 14:30:10 -0300

    Fix authentication problem where passwords were not encoded with bcrypt

--- src/main/java/com/paulograbin/confirmation/security/jwt/NewSecurityConfig.java
+++ src/main/java/com/paulograbin/confirmation/security/jwt/NewSecurityConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -24,15 +26,21 @@ public class NewSecurityConfig {
 
     @Resource
     private JwtTokenAuthorizationOncePerRequestFilter jwtAuthenticationTokenFilter;
-    @Resource
 
+    @Resource
     private UserDetailsService userService;
 
-    //    private String authenticationPath = "/authenticate";
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder, PasswordEncoder passwordEncoder) throws Exception {
+        authenticationManagerBuilder
+                .userDetailsService(userService)
+                .passwordEncoder(passwordEncoder);
     }
 
     @Bean
