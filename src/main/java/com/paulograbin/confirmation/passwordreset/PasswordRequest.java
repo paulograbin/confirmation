fa951e4412b38d56f88d17d70bcc9fb5decd5da2
Paulo Grolli Grabin <paulograbin@gmail.com>
2022-12-19 18:35:12 -0300

    Test this as a solution for Hibernate error after migration

--- src/main/java/com/paulograbin/confirmation/passwordreset/PasswordRequest.java
+++ src/main/java/com/paulograbin/confirmation/passwordreset/PasswordRequest.java
@@ -1,6 +1,7 @@
 package com.paulograbin.confirmation.passwordreset;
 
 import com.paulograbin.confirmation.domain.AbstracEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +23,7 @@ public class PasswordRequest extends AbstracEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(columnDefinition = "uuid", updatable = false)
     private UUID code;
     private String emailAddress;
     private LocalDateTime creationDate;
