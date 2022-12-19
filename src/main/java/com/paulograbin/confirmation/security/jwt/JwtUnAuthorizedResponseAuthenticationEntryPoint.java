f13575a2f97f9dd098f6a0be7afeabdc6a858a82
Paulo Grolli Grabin <paulograbin@gmail.com>
2022-12-19 11:00:03 -0300

    Adapt to Spring Boot 3

--- src/main/java/com/paulograbin/confirmation/security/jwt/JwtUnAuthorizedResponseAuthenticationEntryPoint.java
+++ src/main/java/com/paulograbin/confirmation/security/jwt/JwtUnAuthorizedResponseAuthenticationEntryPoint.java
@@ -1,13 +1,13 @@
 package com.paulograbin.confirmation.security.jwt;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Serializable;
 
