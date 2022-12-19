03603980183ea23624b7707ed019fb812b34a1c8
Paulo Grolli Grabin <paulograbin@gmail.com>
2022-12-19 11:31:59 -0300

    Adapt to Spring Boot 3

--- src/main/java/com/paulograbin/confirmation/security/jwt/JwtUnAuthorizedResponseAuthenticationEntryPoint.java
+++ src/main/java/com/paulograbin/confirmation/security/jwt/JwtUnAuthorizedResponseAuthenticationEntryPoint.java
@@ -19,9 +19,7 @@ public class JwtUnAuthorizedResponseAuthenticationEntryPoint implements Authenti
     private static final Logger log = LoggerFactory.getLogger(JwtUnAuthorizedResponseAuthenticationEntryPoint.class);
 
     @Override
-    public void commence(HttpServletRequest request,
-                         HttpServletResponse response,
-                         AuthenticationException authException) throws IOException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         // This is invoked when user tries to access a secured REST resource without supplying any credentials
         // We should just send a 401 Unauthorized response because there is no 'login page' to redirect to
 
