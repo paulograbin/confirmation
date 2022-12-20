f0d94c8b4c0cda6a04b8bc1fb0dc0ce4f965777d
Paulo Grolli Grabin <paulograbin@gmail.com>
2022-12-20 15:05:56 -0300

    Include proxy for sql statements

---
+++ src/main/java/com/paulograbin/confirmation/ProxyConfiguration.java
@@ -0,0 +1,28 @@
+package com.paulograbin.confirmation;
+
+import net.ttddyy.dsproxy.listener.logging.SLF4JLogLevel;
+import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+
+@Component
+public class ProxyConfiguration implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof DataSource dataSourceBean) {
+            return ProxyDataSourceBuilder.create(dataSourceBean).logQueryBySlf4j(SLF4JLogLevel.INFO).build();
+        }
+
+        return bean;
+    }
+}
